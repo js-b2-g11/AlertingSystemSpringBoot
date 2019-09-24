a5a94417391baa7b21d4990a90da9ad1edfea425
Vrushab Shandilya <vrushab.shandilya@philips.com>
2019-09-25 00:35:54 +0530

    add validators

--- webui/src/app/patient-form/patient-form.component.ts
+++ webui/src/app/patient-form/patient-form.component.ts
@@ -7,6 +7,7 @@ import { Router } from '@angular/router';
 import { PatientComponent } from '../patient/patient.component';
 import { deepEqual } from 'assert';
 import { BedMap } from '../globals';
+import { numberValidator } from '../validators/number-validator';
 
 @Component({
   selector: 'app-patient-form',
@@ -23,34 +24,65 @@ export class PatientFormComponent implements OnInit {
 
   bedKeys: string[];
 
-  patientForm = new FormGroup ({
-    patientId: new FormControl('', Validators.required),
-    bedId: new FormControl(),
-    name: new FormControl(),
-    age: new FormControl(),
-    gender: new FormControl()
-  })
+  patientForm: FormGroup;
+  // patientId: FormControl;
+  // bedId: FormControl;
+  // name: FormControl;
+  // age: FormControl;
+  // gender: FormControl;  
   
-  public constructor(private beds:BedMap, private patientComponent: PatientComponent, private router: Router, private patientService:PatientService) {     
+  public constructor(private beds:BedMap, private patientComponent: PatientComponent, private router: Router, private patientService:PatientService) {
+    this.patientForm = this.createFormGroup();     
   }
 
   ngOnInit() {
+    
     this.getBedArray();    
     this.bedKeys = Object.keys(this.beds.bedMap);    
   }
 
+  createFormGroup() {
+    return new FormGroup ({
+      patientId: new FormControl('', Validators.required),
+      bedId: new FormControl('', Validators.required),
+      name: new FormControl('', Validators.required),
+      age: new FormControl('', [Validators.required, numberValidator]),
+      gender: new FormControl('',[Validators.required])
+    });
+  }
+
   onSubmit() {        
-    // console.log(this.patientForm.get('bedId').value);    
-    this.patientService.savePatient(this.patientForm.value)
+    if (this.patientForm.valid) {
+      this.patientService.savePatient(this.patientForm.value)
       .subscribe( data => {        
         this.beds.bedMap.set(this.patientForm.get('bedId').value, true);
         this.router.navigate(['list-patient']);        
       });
     console.log("Done?");
+    }        
   }
 
   getBedArray() {
     this.bedArray = JSON.parse(window.localStorage.getItem('booleanBed'));    
   }
+
+  get patientId() {
+    return this.patientForm.get('patientId');
+  }
+    
+  get bedId() {
+    return this.patientForm.get('bedId');
+  }
   
+  get name() {
+    return this.patientForm.get('name');
+  }
+
+  get age() {
+    return this.patientForm.get('age');
+  }
+
+  get gender() {
+    return this.patientForm.get('gender');
+  }
 }
