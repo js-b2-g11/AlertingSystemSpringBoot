e7244659ff3578b0f0d8e270a9826b8ee4e36a37
Chiranjeev <jeev.chiran@gmail.com>
2019-09-25 10:00:41 +0530

    rename testcases and reduce cyclomatic complexity of temperature

--- src/test/java/com/philips/bootcamp/TemperatureTest.java
+++ src/test/java/com/philips/bootcamp/TemperatureTest.java
@@ -10,85 +10,85 @@ import com.philips.bootcamp.service.Temperature;
 public class TemperatureTest {
   Temperature temperature = new Temperature();
   @Test
-  public void temperatureValue96() {
+  public void temperatureValue96_notCritical() {
     final boolean actual = temperature.checkRange(96);
     assertEquals(false, actual);
   }
 
   @Test
-  public void temperatureValue97() {
+  public void temperatureValue97_notCritical() {
     final boolean actual = temperature.checkRange(97);
     assertEquals(false, actual);
   }
 
   @Test
-  public void temperatureValue98() {
+  public void temperatureValue98_notCritical() {
     final boolean actual = temperature.checkRange(98);
     assertEquals(false, actual);
   }
 
   @Test
-  public void temperatureValue985() {
+  public void temperatureValue985_notCritical() {
     final boolean actual = temperature.checkRange(98.5f);
     assertEquals(false, actual);
   }
 
   @Test
-  public void temperatureValue995() {
+  public void temperatureValue995_Critical() {
     final boolean actual = temperature.checkRange(99.5f);
     assertEquals(true, actual);
   }
 
   @Test
-  public void temperatureValue1005() {
+  public void temperatureValue1005_Critical() {
     final boolean actual = temperature.checkRange(100.5f);
     assertEquals(true, actual);
   }
 
   @Test
-  public void temperatureValue103() {
+  public void temperatureValue103_Critical() {
     final boolean actual = temperature.checkRange(103);
     assertEquals(true, actual);
   }
 
   @Test
-  public void temperatureValue104() {
+  public void temperatureValue104_Critical() {
     final boolean actual = temperature.checkRange(104);
     assertEquals(true, actual);
   }
 
   @Test
-  public void temperatureValue105() {
+  public void temperatureValue105_Critical() {
     final boolean actual = temperature.checkRange(105);
     assertEquals(true, actual);
   }
 
   @Test
-  public void temperatureValue106() {
+  public void temperatureValue106_Critical() {
     final boolean actual = temperature.checkRange(106);
-    assertEquals(false, actual);
+    assertEquals(true, actual);
   }
 
   @Test
-  public void temperatureValue107() {
+  public void temperatureValue107_Critical() {
     final boolean actual = temperature.checkRange(107);
-    assertEquals(false, actual);
+    assertEquals(true, actual);
   }
 
   @Test
-  public void alertMessageValue985() {
+  public void alertMessageValue985_noAlertMessage() {
     final String actual = temperature.alertMessage(98.5f);
     assertEquals("", actual);
   }
 
   @Test
-  public void alertMessageValue995() {
+  public void alertMessageValue995_noAlertMessage() {
     final String actual = temperature.alertMessage(99.5f);
     assertEquals("", actual);
   }
 
   @Test
-  public void alertMessageValue1005() {
+  public void alertMessageValue1005_alertFever() {
     final String actual = temperature.alertMessage(100.5f);
     assertEquals("Critical Temperature value: Fever", actual);
   }
@@ -100,26 +100,26 @@ public class TemperatureTest {
   }
 
   @Test
-  public void alertMessageValue104() {
+  public void alertMessageValue104_alertHyperpyrexia() {
     final String actual = temperature.alertMessage(104);
     assertEquals("Critical Temperature value: Hyperpyrexia", actual);
   }
 
   @Test
-  public void alertMessageValue105() {
+  public void alertMessageValue105_alertHyperpyrexia() {
     final String actual = temperature.alertMessage(105);
     assertEquals("Critical Temperature value: Hyperpyrexia", actual);
   }
 
   @Test
-  public void alertMessageValue106() {
+  public void alertMessageValue106_alertHyperpyrexia() {
     final String actual = temperature.alertMessage(106);
-    assertEquals("", actual);
+    assertEquals("Critical Temperature value: Hyperpyrexia", actual);
   }
 
   @Test
-  public void alertMessageValue107() {
+  public void alertMessageValue107_alertHyperpyrexia() {
     final String actual = temperature.alertMessage(107);
-    assertEquals("", actual);
+    assertEquals("Critical Temperature value: Hyperpyrexia", actual);
   }
 }
